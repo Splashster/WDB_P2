546dca6aa1a92300e8c4ac350e8dbdfda602506c
coursework <coursework@coursework.csc.tntech.edu>
2017-03-09 15:19:07 -0600

    Finishing up gerneate basex query

--- cgi-bin/bibfinder2.py
+++ cgi-bin/bibfinder2.py
@@ -15,36 +15,56 @@ title_content = form_items.getvalue('title_content')
 search_type = form_items.getvalue('searchtype')
 abstract_content = form_items.getvalue('abstract_content')
 multiple_items = False
-fullname = ""
 #auth_checkbox = "on"
 #author = "Frank G G"
 
 def generateBaseXQuery():
 	
-	xpath = 'MedlineCitationSet//MedlineCitationi//Article['
+	xpath = 'MedlineCitationSet//MedlineCitation//Article['
+	multiple_items = False
 
 	if auth_checkbox:
-		multiple_items = True
 		try:
 			fullname = author.split()
-			forename = fullname[0] + " " + fullname[1]
-			lastname = fullname[2]
+			if len(fullname) == 3:
+				forename = fullname[0] + " " + fullname[1]
+				lastname = fullname[2]
+			elif len(fullname) == 2:
+				forename = fullname[0]
+				lastname = fullname[1]
+			elif len(fullname) == 1:
+				forename = fullname[0]
+				lastname = ""
 		except:
 			forename = ""
 			lastname = ""
-		xpath += 'AuthorList/Author[LastName=' + lastname + ' and ForName=' + forename 
-	
-	if title_checkbox:
-		search_items['Title_Contents'] = title_content
-	if type_checkbox:
-		search_items['Publication_Type'] = search_type  
-	if abstract_checkbox:
-		search_items['Abstract_Contents'] = abstract_content
 
-	xpath += ']/ArticleTitle'
+		xpath += "AuthorList/Author[LastName= '{0}' and ForeName='{1}']".format(lastname,forename) 
+		multiple_items = True
+	if title_checkbox:
+		if multiple_items:
+			xpath += " and contains(ArticleTitle,'{0}')".format(title_content)
+		else:
+			multiple_items = True
+			xpath += "contains(ArticleTitle,'{0}')".format(title_content)
+	if type_checkbox:
+		if multiple_items:
+			xpath += " and PublicationTypeList='{0}'".format(search_type)
+		else:
+			multiple_items = True
+			xpath += "PublicationTypeList='{0}'".format(search_type)
+	if abstract_checkbox:
+		if multiple_items:
+			xpath += " and contains(Abstract,'{0}')".format(abstract_content)
+		else:
+			multiple_items = True
+			xpath += "contains(Abstract,'{0}')".format(abstract_content)
+
+	xpath += ']'
+	xpath += '/PublicationTypeList/PublicationType | ' + xpath + '/AuthorList/Author | ' + xpath + '/ArticleTitle | ' + xpath + '/Abstract/AbstractText'
 
 	query = 'htttp://admin:admin@localhost:8984/rest/medsamp2012?query=distinct-values(data('+xpath+'))'
-	return xpath
+	return query
 
 art = generateBaseXQuery()
 print """Content-type:text/html\r\n\r\n
