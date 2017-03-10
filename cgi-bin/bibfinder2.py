914cfbdbe35a874f91e217c1dcb3a20f266c7e2a
coursework <coursework@coursework.csc.tntech.edu>
2017-03-10 12:51:00 -0600

    Adding existdb stuff

--- cgi-bin/bibfinder2.py
+++ cgi-bin/bibfinder2.py
@@ -6,6 +6,7 @@ import requests
 import xml.etree.ElementTree as ET
 from lxml import etree
 
+cgi.FieldStorage
 form_items = cgi.FieldStorage()
 auth_checkbox = form_items.getvalue('auth_search')
 title_checkbox = form_items.getvalue('title_con')
@@ -16,13 +17,14 @@ title_content = form_items.getvalue('title_content')
 search_type = form_items.getvalue('searchtype')
 abstract_content = form_items.getvalue('abstract_content')
 responses = []
+publication_type_mapping = {'Journal Article':'0', 'Book':'1', 'In a conference proceedings':'3', 'In a collection (part of a book but has its own title)':'5','Tech Report':'10', 'Unpublished':'13', 'Miscellaneous':'16','In a conference proceedings':'47'}
 #abstract_checkbox = "on"
 #abstract_content = "Photolytic"
 type_checkbox = "on"
 search_type = "Journal Article"
 main_list = []
-msg = ""
-
+baseXMessage = ""
+existDBMessage = ""
 
 def parseResponse(response, response_type):
 	auth_list = []
@@ -36,12 +38,8 @@ def parseResponse(response, response_type):
 
 	if response_type == 'baseX':
 		responses = response.text.split("<ArticleTitle>")
-		#print tree
-		#print etree.tostring(tree)
-		#print responses
 		for items in responses:
 			tree = "<root>" + "<ArticleTitle>" + items + "</root>"
-			#print tree
 			tree = etree.fromstring(tree,parser)
 			root = ET.fromstring(etree.tostring(tree))
 			if items != "":
@@ -50,40 +48,24 @@ def parseResponse(response, response_type):
 					title = first_slice[0]
 				else:
 					title = "NONE"
-				#print title
 				if "<Abstract>" in items:
-					#second_slice = first_slice[1].split("<AuthorList")	
-					#tree = ET.fromstring(second_slice[0])
-									#print root[1][0].text
 					for child in root.find('Abstract'):
 						abstract = child.text
 						abs_list.append(abstract)
-						#printi abstract
-					#print root.tag
 				else:
 					abs_list.append("NONE")
-				#print abs_list
-				#print str(abstracts)
-				#third_slice =  second_slice[1].split("<PublicationTypeList>")
-				#third_slice[0] =  "<AuthorList" + third_slice[0]
-				#tree = ET.fromstring(third_slice[0])
 				if "<AuthorList" in items:
 					for child in root.find("AuthorList"):
 						try:	
 							fullname = child.find("./ForeName").text + " " +  child.find("./LastName").text
 							auth_list.append(fullname)
-							#print fullname
 						except:
 							pass
-				#	print auth_list
 				else:
 					auth_list.append("NONE")
 				if "<PublicationTypeList" in items:
-				#fourth_slice = "<PublicationTypeList>" + third_slice[1]
-				#tree = ET.fromstring(fourth_slice)
 					for child in root.find("PublicationTypeList"):
 						types_list.append(child.text)
-					#print types_list
 				else:
 					types_list.append("NONE")
 				parts_list.append(types_list)
@@ -91,8 +73,6 @@ def parseResponse(response, response_type):
 				parts_list.append(str(title))
 				parts_list.append(abs_list)
 				main_list.append(parts_list)
-				#print main_list
-				#print "*****************************************************"
 				auth_list = []
 				types_list = []
 				abs_list = []
@@ -119,16 +99,90 @@ def parseResponse(response, response_type):
 						message+='*********************************************<br>'
 						item_count +=1
 	
-				global msg 
-				msg = message
+				global baseXMessage 
+'''				baseXMessage = message
+	else:
+		root = ET.fromstring(response)
+			tree = "<root>" + "<ArticleTitle>" + items + "</root>"
+			tree = etree.fromstring(tree,parser)
+			root = ET.fromstring(etree.tostring(tree))
+			if items != "":
+				if "</ArticleTitle>" in items:
+					first_slice = items.split("</ArticleTitle>")
+					title = first_slice[0]
+				else:
+					title = "NONE"
+				if "<Abstract>" in items:
+					for child in root.find('Abstract'):
+						abstract = child.text
+						abs_list.append(abstract)
+				else:
+					abs_list.append("NONE")
+				if "<AuthorList" in items:
+					for child in root.find("AuthorList"):
+						try:	
+							fullname = child.find("./ForeName").text + " " +  child.find("./LastName").text
+							auth_list.append(fullname)
+						except:
+							pass
+				else:
+					auth_list.append("NONE")
+				if "<PublicationTypeList" in items:
+					for child in root.find("PublicationTypeList"):
+						types_list.append(child.text)
+				else:
+					types_list.append("NONE")
+				parts_list.append(types_list)
+				parts_list.append(auth_list)
+				parts_list.append(str(title))
+				parts_list.append(abs_list)
+				main_list.append(parts_list)
+				auth_list = []
+				types_list = []
+				abs_list = []
+				parts_list = []
+				message = ""
+				for item in main_list:
+					for parts in item:
+						if count == 0:
+							for i in parts:
+								message += 'ReferenceType: {0}<br>'.format(i.encode('utf-8'))
+							count = 1
+						elif count == 1:
+							for i in parts:
+								message += 'Authors: {0}<br>'.format(i.encode('utf-8'))
+							count = 2
+						elif count == 2:
+							message += 'Title: {0}<br>'.format(parts.encode('utf-8'))
+							count = 3
+						elif count == 3:
+							for i in parts:
+								message += 'Abstract: {0}<br>'.format(i.encode('utf-8'))
+							count = 0
+					if len(main_list) > 1 and item_count < len(main_list):
+						message+='*********************************************<br>'
+						item_count +=1
+	
+				global baseXMessage 
+				baseXMessage = message
+
+'''
+
 def sendQuery(query, response_type):
 	#print query
 	response = requests.get(query)
-	if response.text != "":
-		parseResponse(response, response_type)
+	if response_type == 'baseX':
+		if response.text != "":
+			parseResponse(response, response_type)
+		else:
+			global baseXMessage 
+			baseXMessage = "NONE"
 	else:
-		global msg 
-		msg = "NONE"
+		if "exist:hits='0'" not in response.text:
+			parseResponse(response, response_type)
+		else:
+			global existDBMessage
+			existDBMessage = "NONE"
 def generateBaseXQuery():
 	
 	xpath = 'MedlineCitationSet//MedlineCitation//Article['
@@ -177,9 +231,49 @@ def generateBaseXQuery():
 		query = 'http://admin:admin@localhost:8984/rest/medsamp2012?query='+xpath+''
 		sendQuery(query, 'baseX')
 	else:
-		global msg 
-		msg = "NONE"		
+		global baseXMessage 
+		baseXMessage = "NONE"		
+
+def generateExistDBQuery():
+	
+	xpath = '//XML/RECORDS//RECORD['
+	
+	oneChecked = False
+	if auth_checkbox:
+
+		oneChecked = True
+		xpath += "AUTHORS[AUTHOR= '{0}']".format(author)
+	if title_checkbox:
+		if oneChecked:
+			xpath += " and contains(TITLE,'{0}')".format(title_content)
+ 		else:
+			oneChecked = True
+			xpath += "contains(TITLE,'{0}')".format(title_content)
+	if type_checkbox:
+		ref_val = publication_type_mapping.get(search_type) 
+		if oneChecked:
+			xpath += " and REFERENCE_TYPE='{0}'".format(ref_val)
+		else:
+			oneChecked = True
+			xpath += "REFERENCE_TYPE='{0}'".format(ref_val)
+	if abstract_checkbox:
+		if oneChecked:
+			xpath += " and contains(ABSTRACT,'{0}')".format(abstract_content)
+		else:
+			oneChecked = True
+			xpath += "contains(ABSTRACT,'{0}')".format(abstract_content)
+	
+	if oneChecked: 
+		xpath += "]"
+		#xpath += '/REFERENCE_TYPE|' + xpath + '/AUTHORS|' + xpath + '/TITLE|' + xpath + '/ABSTRACT'  	
+		query = 'http://localhost:8080/exist/rest/db/acm-turing-awards?_query='+xpath+'&_howmany=10000'
+		sendQuery(query, 'existDB')
+	else:
+		global existDBMessage 
+		existDBMessage = "NONE"		
+	print query
 generateBaseXQuery()
+generateExistDBQuery()
 #print msg
 #print main_list
 print """Content-type:text/html\r\n\r\n
@@ -188,5 +282,5 @@ print """Content-type:text/html\r\n\r\n
 <script>console.log({0});</script>
 </body>
 </html>
-""".format(msg)
+""".format("s")
 #print msg
